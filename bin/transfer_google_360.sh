b608ed81631d6fde72a7b106fb2645c5ef1bf66e
yun.song <yun.song1989@gmail.com>
2016-11-02 10:39:47 +0800

    更新字体源

--- bin/transfer_google_360.sh
+++ bin/transfer_google_360.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-sed -i "s/https:\/\/fonts.googleapis.com/http:\/\/fonts.useso.com/g" `find ./vendor -type f -name "*.css"`
-sed -i "s/https:\/\/fonts.googleapis.com/http:\/\/fonts.useso.com/g" `find ./vendor -type f -name "*.less"`
+sed -i "s/https:\/\/fonts.googleapis.com/http:\/\/fonts.css.network/g" `find ./vendor -type f -name "*.css"`
+sed -i "s/https:\/\/fonts.googleapis.com/http:\/\/fonts.css.network/g" `find ./vendor -type f -name "*.less"`
+
+sed -i "s/http:\/\/fonts.useso.com/https:\/\/fonts.css.network/g" `find ./vendor -type f -name "*.css"`
+sed -i "s/http:\/\/fonts.useso.com/https:\/\/fonts.css.network/g" `find ./vendor -type f -name "*.less"`
