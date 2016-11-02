8d95e7df7c2feebb2ed9c98a25c390355e9980bd
yun.song <yun.song1989@gmail.com>
2016-11-02 10:51:11 +0800

    migration表创建

---
+++ console/migrations/m161101_100314_create_project_manage.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+class m161101_100314_create_project_manage extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%project_manage}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string(64)->notNull(),
+            'appid' => $this->integer()->notNull()->unique(),
+            'appkey' => $this->string(64)->notNull(),
+            'status' => $this->smallInteger()->notNull()->comment("项目是否可用 1可用 0 不可用")->defaultValue(1),
+            'desc' => $this->string()->notNull()->comment("注释"),
+            'belong_uid' => $this->integer()->notNull()->comment("项目所属用户"),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%project_manage}}');
+    }
+
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+
+    public function safeDown()
+    {
+    }
+    */
+}
---
+++ console/migrations/m161101_102358_create_blacklist_manage.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m161101_102358_create_blacklist_manage extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+
+        $this->createTable('{{%blacklist_manage}}', [
+            'id' => $this->primaryKey(),
+            'appid' => $this->integer()->notNull(),
+            'content' => $this->string(20)->notNull()->comment("黑名单(ip or uid)"),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer()->notNull(),
+        ]);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%blacklist_manage}}');
+        return false;
+    }
+
+}
