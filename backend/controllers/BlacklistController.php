8f773c18e38f9685fb320fb6cd8cb27f45c34a0b
yun.song <yun.song1989@gmail.com>
2016-11-09 11:49:09 +0800

    项目管理，黑名单管理，继承关系调整

--- backend/controllers/BlacklistController.php
+++ backend/controllers/BlacklistController.php
@@ -5,7 +5,6 @@ namespace backend\controllers;
 use Yii;
 use common\models\Blacklist;
 use common\models\BlacklistSearch;
-use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 
